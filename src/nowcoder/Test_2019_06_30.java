10692cca7c9e7533eb243ee3e097e66b966d3817
1771979508 <1771979508@qq.com>
2019-07-01 21:38:47 +0800

    牛客网练习

---
+++ src/nowcoder/Test_2019_06_30.java
@@ -0,0 +1,30 @@
+package nowcoder;
+/**
+*	@author 作者 Joker青
+*	@version 创建时间：2019年6月30日 下午9:49:44	
+*/
+public class Test_2019_06_30 {
+	
+	public static void main(String[] args) {
+		
+//		String string = 
+//				"";
+//		String[] strings = string.split(",");
+//		for(String s : strings){
+//			System.out.println(s);
+//		}
+//		System.out.print(strings.length);
+		
+		Integer i01=59;
+		int i02=59;
+		Integer i03=Integer.valueOf(59);
+		Integer i04=new Integer(59);
+		System.out.println(i01==i02);
+		System.out.println(i01==i03);
+		System.out.println(i03==i04);
+		System.out.println(i02==i04);
+
+	}
+	
+}
+	
